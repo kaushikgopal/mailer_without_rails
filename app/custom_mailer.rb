08379949505a94b427333ebaa2ca6cf3298c3408
Kaushik Gopal <code@kaush.co>
2013-06-07 00:13:06 -0700

    Now using YAML files

--- app/custom_mailer.rb
+++ app/custom_mailer.rb
@@ -1,29 +1,22 @@
 require "action_mailer"
 
+
+EMAIL = YAML.load_file(Dir.pwd + '/app/email_address.yml')
+
+ActionMailer::Base.raise_delivery_errors = true
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = YAML.load_file(Dir.pwd + '/app/smtp.yml')
+ActionMailer::Base.view_paths =  File.expand_path('../views/', __FILE__)
+
+
 class CustomMailer < ActionMailer::Base
-	def notification(to_email, from_email = "from_email@example.com")
-		mail(to: to_email, from: from_email, subject: "Email notification through Ruby") do |format|
+	def notification(to_email, from_email = EMAIL[:from])
+		mail(to: to_email, from: from_email, subject: "Email notification through Ruby - now with YAML") do |format|
 			format.text
 			format.html
 		end
 	end
-
 end
 
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.delivery_method = :smtp
 
-ActionMailer::Base.smtp_settings = {
-	:address 		=> "smtp.gmail.com",
-	:port 			=> 587,
-	:domain			 => "domain",
-	:authentication => :plain,
-	:user_name => "username",
-	:password => "password",
-  :enable_starttls_auto => true
-}
-
-
-ActionMailer::Base.view_paths =  File.expand_path('../../app/views/', __FILE__)
-
-CustomMailer.notification("sendemail@example.com").deliver
+CustomMailer.notification(EMAIL[:to]).deliver
